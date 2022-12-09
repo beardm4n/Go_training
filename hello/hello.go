a51e158c9b974df9c61cd5894f4cc8d6792c7946
beardm4n <beardm4n@elo.pub>
2022-12-10 00:29:26 +0300

    added error handler

--- hello/hello.go
+++ hello/hello.go
@@ -4,12 +4,32 @@ import (
     "fmt"
     "rsc.io/quote"
     "go_training/greetings"
+    "log"
 )
 
 func main() {
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
     fmt.Println(quote.Go())
 
-    message := greetings.Hello("Amid")
+    message1, err1 := greetings.Hello("Amid")
 
-    fmt.Println(message)
+    if err1 != nil {
+        log.Fatal(err1)
+    }
+
+    fmt.Println(message1)
+    fmt.Println("===================================")
+
+    message2, err2 := greetings.Hello("")
+
+    if err2 != nil {
+        log.Fatal(err2)
+    }
+
+    fmt.Println(message2)
 }
